b417806407e96b863986aa4b5a7c49af67cd6a86
Kharis <Kharisrahmat686@gmail.com>
2022-05-25 12:26:01 +0700

    menambahkan route home

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
